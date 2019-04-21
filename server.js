6d8bd5a61f0354e268ef7e78111e7e4e6394270e
sali7943 <47040095+sali7943@users.noreply.github.com>
2019-04-21 01:48:05 -0600

    Update server.js

--- server.js
+++ server.js
@@ -62,64 +62,6 @@ var smtpTransport = nodemailer.createTransport({
 });
 var rand, mailOptions, host, link;
 
-// Begin Email Routing
-app.get('/send', function(req, res){
-    rand=Math.floor((Math.random() * 100) + 54);
-	host=req.get('host');
-	link="http://"+req.get('host')+"/verify?id="+rand;
-	mailOptions={
-        from: 'forte.music.help@gmail.com', //sender address
-		to : req.query.to, // Reciever
-		subject : "Please confirm your Email account",
-		html : "Hello,<br> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>"	
-	}
-	console.log(mailOptions);
-	smtpTransport.sendMail(mailOptions, function(error, response){
-   	 if(error){
-        	console.log(error);
-		res.end("error");
-	 }else{
-        	console.log("Message sent: " + response.message);
-		res.end("sent");
-         }
-    });
-});
-
-// Veritfy Email
-app.get('/verify',function(req,res){
-    console.log(req.protocol+":/"+req.get('host'));
-
-    if((req.protocol+"://"+req.get('host'))==("http://"+host))
-    {
-        console.log("Domain is matched. Information is from Authentic email");
-        if(req.query.id==rand)
-        {
-            // User's account updated in database to verified
-            console.log("email is verified");
-            res.end("<h1>Email "+mailOptions.to+" is been Successfully verified");
-
-            db.tx(v => {
-                return v.none('UPDATE users SET verified = $1 WHERE email = $2', [true, mailOptions.to]);
-            })
-            .then(data => {
-                //success
-            })
-            .catch(error =>{
-                console.log('ERROR:', error);
-            });
-        }
-        else
-        {
-            console.log("email is not verified");
-            res.end("<h1>Bad Request</h1>");
-        }
-    }
-    else
-    {
-        res.end("<h1>Request is from unknown source");
-    }
-});
-
 // Passport Serialize/Deserialize
 passport.serializeUser((user, done) => {
     done(null, user.id);
